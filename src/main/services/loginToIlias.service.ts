d1e3c27271b7b13370471e97c2907f069233b4f1
Paul <paul@pabr.de>
2024-03-07 16:07:11 +0100

    fiddle with login

--- src/main/services/loginToIlias.service.ts
+++ src/main/services/loginToIlias.service.ts
@@ -8,35 +8,36 @@ async function login(): Promise<string> {
         return 'Please enter all required fields in the settings.';
     }
 
-    console.log(url, username, password, webdavId);
+    // console.log(url, username, password, webdavId);
 
     // Login
-    const login = await axios.post(url + `/ilias.php?lang=de&client_id=${webdavId}&cmd=post&cmdClass=ilstartupgui&baseClass=ilStartUpGUI&rtoken=`, {
+    const login = await axios.post(url + `/ilias.php?lang=de&client_id=${webdavId}&cmd=post&cmdClass=ilstartupgui&cmdNode=10l&baseClass=ilStartUpGUI&rtoken=`, {
         "username": username,
         "password": password,
-        // "cmd[doStandardAuthentication]": 'Anmelden'
+        "cmd[doStandardAuthentication]": 'Anmelden'
     }, {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        maxRedirects: 4,
-        validateStatus: function (status) {
-            return status < 305; // Resolve only if the status code is less than 300
-        }
+        maxRedirects: 10
     });
 
     // console.log(login.data);
+    console.log(login.request._redirectable._redirectCount);
     // console.log(login.data, login.statusText, login.status);
 
     // //search if data contains password input field
     if (login.data.includes('password')) {
         console.log('Invalid login data');
-        return 'Invalid login data';
+        // return 'Invalid login data';
     }
 
     // Get courses
-    // let courses = await axios.get(url + '/ilias.php?baseClass=ilDashboardGUI&cmd=jumpToSelectedItems');
-    // console.log(courses.data, courses.status);
+    let courses = await axios.get(url + '/ilias.php?baseClass=ilDashboardGUI&cmd=jumpToSelectedItems');
+    if(courses.data.includes('ref_id')) {
+        console.log('Logged in');
+        return 'Logged in';
+    }
 
     return 'changeme';
 }
