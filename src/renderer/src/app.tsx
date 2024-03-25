cabc1995c9720fa8fd80f069bf22f1ac220f82ac
Paul <paul@pabr.de>
2024-03-25 16:25:34 +0100

    try to fix build problem

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -2,19 +2,19 @@ import Header from './components/header.component';
 import { Route, Routes } from 'react-router-dom';
 import SettingsPage from './pages/settings.page';
 import Dashboard from './pages/dashboard.page';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AboutPage from './pages/about.page';
 
 function App() {
     return (
-        <Router>
+        <BrowserRouter>
             <Header />
             <Routes>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/settings" element={<SettingsPage />} />
                 <Route path="/about" element={<AboutPage />} />
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
 
